5a220fe782b91b117cd68f9055bf260f603025bd
seogi_macbook <sgkang09@github.com>
2015-11-01 22:36:06 -0800

    IPython Gui for ZarcFit

--- codes/ColeCole.py
+++ codes/ColeCole.py
@@ -1,27 +1,47 @@
-class ModifiedColeCole(object):
-	"""docstring for ModifiedColeCole"""
-#Randy has intervened here! 2015-10-15
+class Zarcfit(object):
+	"""docstring for Zarcfit"""
+
 	data = None
-	Rm = None
+	Linf = None
+	R0 = None 
+	Rh = None 
+	Qh = None
+	Ph = None	
+	Rm = None 
 	Qm = None
-	ss
+	Pm = None
+	Rl = None
+	Ql = None
+	Pl = None
+	Re = None
+	Qe = None
+	Pef = None	
+	Pei = None	
+
 	def __init__(self, data, **kwargs):
 		self.data = data
-
-	def ReadParams(self):
-		self.Rm = data['Rm'].values 
-		self.Qm = data['Qm'].values
-		self.Pm = data['Pm'].values
-		self.Rl = data['Rl'].values 
-		self.Ql = data['Ql'].values
-		self.Pl = data['Pl'].values
+	# def ReadParams(self):
+	# 	self.LabID = data['LabID'].values 
+	# 	self.Rm = data['Rm'].values 
+	# 	self.Qm = data['Qm'].values
+	# 	self.Pm = data['Pm'].values
+	# 	self.Rl = data['Rl'].values 
+	# 	self.Ql = data['Ql'].values
+	# 	self.Pl = data['Pl'].values
 
 	def CPEfun(Rx, Qx, px, freq):
-	    
 	    out = np.zeros_like(freq, dtype=complex128)
-	    out = (Rx*Qx*(1j*np.pi*2*freq)**px+1.)/Rx
-	    out = 1./(1./Rx + 1j*Qx*(np.pi*2*freq)**px)
+	    out = 1./(1./Rx + Qx*(np.pi*2*freq*1j)**px)
+	    return out
 
+	def CPEfunElec(Rx, Qx, pex, pix, freq):
+	    out = np.zeros_like(freq, dtype=complex128)
+	    out = 1./(1./Rx + (1j)**pix*Qx*(np.pi*2*freq)**pex)
+	    return out
+
+	def CPEfunSeries(Rx, Qx, px, freq):
+	    out = np.zeros_like(freq, dtype=complex128)
+	    out = Rx + 1./(Qx*(np.pi*2*freq*1j)**px)
 	    return out
 
 
